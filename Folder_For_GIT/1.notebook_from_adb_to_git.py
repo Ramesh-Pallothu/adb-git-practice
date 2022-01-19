00007eacaac70132b388d7250ace41b79fbb653d
Ramesh-Pallothu <98036969+Ramesh-Pallothu@users.noreply.github.com>
2022-01-20 00:17:20 +0530

    Creating another conflic

--- Folder_For_GIT/1.notebook_from_adb_to_git.py
+++ Folder_For_GIT/1.notebook_from_adb_to_git.py
@@ -5,4 +5,4 @@
 #This line is from the git edit
 #This line is just in the note book
 # There is another line added in the notebook but not committed from there. These two new lines are to be conflict
-
+#A new line is added to the notebook in adb. Something like this - "Another line in not in git but in notebook alone."
