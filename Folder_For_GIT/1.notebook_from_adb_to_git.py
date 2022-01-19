f76bb17ceb4f93f597f94c74cfa928bbb69a2af3
Ramesh-Pallothu <98036969+Ramesh-Pallothu@users.noreply.github.com>
2022-01-19 23:56:21 +0530

    Trying to create a conflict situation

--- Folder_For_GIT/1.notebook_from_adb_to_git.py
+++ Folder_For_GIT/1.notebook_from_adb_to_git.py
@@ -3,3 +3,4 @@
 # added line
 #Line added from db
 #This line is from the git edit
+# There is another line added in the notebook but not committed from there. These two new lines are to be conflict
