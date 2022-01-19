3f563d30b3c1c8f997c55ed4f50990b277643046
rpallothu1@gmail.com <rpallothu1@gmail.com>
2022-01-19 18:49:36 +0000

    Conflict 2 msg

--- Folder_For_GIT/1.notebook_from_adb_to_git.py
+++ Folder_For_GIT/1.notebook_from_adb_to_git.py
@@ -5,4 +5,5 @@
 #This line is from the git edit
 #This line is just in the note book
 # There is another line added in the notebook but not committed from there. These two new lines are to be conflict
+#Another line in not in git but in notebook alone. Created to try conflict resolution
 
